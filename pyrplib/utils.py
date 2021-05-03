a790ddb7f78a9540a444b3d03f8b1e0efa28dd3c
Arman Rafian <arafian@calpoly.edu>
2021-05-03 01:22:46 -0400

    Revert "Add compute features"
    This reverts commit f7a3e593b2b726b4bc6da7c2be9627d149a7a57b.

--- pyrplib/utils.py
+++ pyrplib/utils.py
@@ -3,31 +3,16 @@
 
 # # A Utility Library for Rankability Problems
 
-# In[7]:
+# In[ ]:
 
 
 import pandas as pd
 import itertools
-import numpy as np
-
-
-# In[6]:
-
-
-import sys
-from pathlib import Path
-home = str(Path.home())
-
-sys.path.insert(0,"%s/rankability_toolbox_dev"%home)
-sys.path.insert(0,"%s/RPLib"%home)
-import pyrankability
 
 
 # ## Problem 1
 
-# ### Generate
-
-# In[3]:
+# In[ ]:
 
 
 # Constructs a dataframe from selected parameters
@@ -44,7 +29,7 @@ def get_sel_df(columns):
     return sel_df
 
 
-# In[4]:
+# In[ ]:
 
 
 def filter_teams(games, remaining_games, teams_by_year):
@@ -58,49 +43,4 @@ def filter_teams(games, remaining_games, teams_by_year):
         remaining_games[year] = remaining_games[year].loc[team1_name.isin(teams) | team2_name.isin(teams)]
         
     return games, remaining_games
-
-
-# ### Baseline
-
-# In[5]:
-
-
-def compute_features(D,rankings,top_k,feature_columns):
-    top_teams = list(rankings.sort_values().index[:top_k])
-    D = D.loc[top_teams,top_teams]
-    delta_lop,details_lop = pyrankability.rank.solve(D.fillna(0),method='lop',cont=True)
-
-    x = pd.DataFrame(details_lop['x'],index=D.index,columns=D.columns)
-    r = x.sum(axis=0)
-    order = np.argsort(r)
-    xstar = x.iloc[order,:].iloc[:,order]
-    xstar.loc[:,:] = pyrankability.common.threshold_x(xstar.values)
-    inxs = np.triu_indices(len(xstar),k=1)
-    xstar_upper = xstar.values[inxs[0],inxs[1]]
-    nfrac_upper_lop = sum((xstar_upper > 0) & (xstar_upper < 1))
-    
-    top_teams = xstar.columns[:top_k]
-    
-    k_two_distant,details_two_distant = pyrankability.search.solve_pair(D.fillna(0),method='lop',minimize=False,verbose=False)
-    d_lop = k_two_distant#details_two_distant['tau']
-    
-    delta_hillside,details_hillside = pyrankability.rank.solve(D,method='hillside',cont=True)
-    
-    x = pd.DataFrame(details_hillside['x'],index=D.index,columns=D.columns)
-    r = x.sum(axis=0)
-    order = np.argsort(r)
-    xstar = x.iloc[order,:].iloc[:,order]
-    xstar.loc[:,:] = pyrankability.common.threshold_x(xstar.values)
-    inxs = np.triu_indices(len(xstar),k=1)
-    xstar_upper = xstar.values[inxs[0],inxs[1]]
-    nfrac_upper_hillside = sum((xstar_upper > 0) & (xstar_upper < 1))
-    
-    top_teams = xstar.columns[:top_k]
-    
-    k_two_distant,details_two_distant = pyrankability.search.solve_pair(D,method='hillside',minimize=False,verbose=False)
-    d_hillside = k_two_distant#details_two_distant['tau']
-    
-    features = pd.Series([delta_lop,delta_hillside,2*nfrac_upper_lop,2*nfrac_upper_hillside,d_lop,d_hillside],index=feature_columns)
-
-    return features
 
