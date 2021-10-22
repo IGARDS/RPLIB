c0ab9c5869264a2415fb749a6ada2ce2c94ca780
Paul Anderson <pauleanderson@gmail.com>
2021-10-22 18:06:04 -0400

    update

--- dash/run.sh
+++ dash/run.sh
@@ -1,1 +1,10 @@
-gunicorn app:server --reload -b :7001
+#!/bin/bash
+
+if [ "$1" == "" ] || [ $# -gt 1 ]; then
+    echo PORT is not specified;
+    exit 1
+fi
+
+PORT=$1
+
+gunicorn app:server --reload -b :$PORT
