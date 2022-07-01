21ef595f717ea5b526faff888814703d0b0facb6
Paul Anderson <pauleanderson@gmail.com>
2022-07-01 16:17:52 -0400

    cleanup

--- setup.py
+++ setup.py
@@ -1,28 +1,4 @@
 from setuptools import setup
-
-setup(name='pyrplib',
-      version='0.1',
-      description='RPLIB Python Library',
-      url='https://github.com/IGARDS/RPLIB',
-      author='Paul Anderson, Brandon Tat, Charlie Ward',
-      author_email='pauleanderson',
-      license='MIT',
-      install_requires=[
-          'pytest',
-          'dash',
-          'matplotlib',
-          'nx_altair',
-          'pygraphviz',
-          'dash_bootstrap_components',
-          'requests',
-          'pyrankability',
-          'joblib'
-      ],
-      packages=['pyrplib'],
-      zip_safe=False)
-
-
-from setuptools import setup
 from pathlib import Path
 
 this_directory = Path(__file__).parent
